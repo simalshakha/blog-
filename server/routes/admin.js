3ec383bba1d5a088a9fa475a8cd85be6aa3d244c
simalshakha <sshakha350@gmail.com>
2025-05-04 00:05:46 +0545

    dashboard

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -66,15 +66,25 @@ router.post('/admin', async (req, res) => {
         return res.status(500).send('Internal Server Error');
     }
 });
-router.get('/dashboard',authMiddleware,async(req,res)=>{
-    try{
+router.get('/dashboard', authMiddleware, async (req, res) => {
+    try {
+        const locals = {
+        title: 'Dashboard',
+        description: 'Simple Blog created with NodeJs, Express & MongoDb.'
+    }
 
-        const data=await post.find();
-        res.render('/admin/dashboard',{data});
+        const data = await Post.find();
+        res.render('admin/dashboard', {
+        locals,
+        data,
+        layout: adminLayout
+    });
 
-    }catch(error){
-    res.render('admin/dashboard');}
-})
+    } catch (error) {
+        console.log(error);
+    }
+
+});
 
 
 router.get('/add-post',authMiddleware,async(req,res)=>{
@@ -90,26 +100,6 @@ router.get('/add-post',authMiddleware,async(req,res)=>{
     res.render('admin/dashboard');}
 })
 
-
-router.get('/edit-post/:id', authMiddleware, async (req, res) => {
-    try {
-
-        const locals = {
-        title: "Edit Post",
-        description: "Free NodeJs User Management System",
-        };
-
-        const data = await Post.findOne({ _id: req.params.id });
-
-        res.render('admin/edit-post', {
-            locals,
-            data,
-            layout: adminLayout
-        })
-
-    } catch (error) {
-        console.log(error);
-    }
 
 router.post('/register', async (req, res) => {
     try {
@@ -133,6 +123,23 @@ router.post('/register', async (req, res) => {
         res.redirect('/admin')
     }
 });
+
+router.put('/edit-post/:id', authMiddleware, async (req, res) => {
+    try {
+  
+      await Post.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        body: req.body.body,
+        updatedAt: Date.now()
+      });
+  
+      res.redirect(`/edit-post/${req.params.id}`);
+  
+    } catch (error) {
+      console.log(error);
+    }
+  
+  });
 
 router.delete('/delete-post/:id', authMiddleware, async (req, res) => {
 
